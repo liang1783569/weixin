66dfd4947fc9611b1a359e8eabafc5c5985cb653
liang1783569 <1453823062@qq.com>
2020-01-06 22:01:56 +0800

    hzl 2020-1-6

--- components/at-home/at-home.js
+++ components/at-home/at-home.js
@@ -1,11 +1,18 @@
 Component({
+  behaviors: [],
   data:{
     list:''
+  },
+  properties: {
+    url: String,
+    title:String,
+    bannerimg:String
+
   },
   attached(){
     let that = this
     wx.request({
-      url: 'http://m.haimi.com/api/nav/advert-team-products?NavID=3&page=1&pageSize=100&platform=WAP', //仅为示例，并非真实的接口地址
+      url: this.properties.url, //仅为示例，并非真实的接口地址
       header: {
         'content-type': 'application/json' // 默认值
       },
